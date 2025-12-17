f5cd16c722a20eeb1eb49f455802257e2d57d74f
francescofiorentinoTIM <francesco.fiorentino@telecomitalia.it>
2025-12-17 10:21:21 +0100

    TIM-FIX

--- api/federation/client/client.gen.go
+++ api/federation/client/client.gen.go
@@ -2924,8 +2924,8 @@ func (r RemoveAppResponse) StatusCode() int {
 }
 
 type AppInstanceDetailsJSON200Response struct {
-	// AccesspointInfo Information about the IP and Port exposed by the OP. Application clients shall use these access points to reach this application instance.
-	AccesspointInfo []AccessPointInfo `json:"accessPointInfo,omitempty"`
+	// AccessPointInfo Information about the IP and Port exposed by the OP. Application clients shall use these access points to reach this application instance.
+	AccessPointInfo []AccessPointInfo `json:"accessPointInfo,omitempty"`
 
 	// AppInstanceState Running status of the application instance.
 	AppInstanceState *InstanceState `json:"appInstanceState,omitempty"`
